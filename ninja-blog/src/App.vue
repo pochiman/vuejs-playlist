1a1bf77b090e3b3de4e0b54fc3a2b7e4295cc96a
pochiman <pochiman@yahoo.com>
2020-03-15 18:53:55 -0400

    30

---
+++ ninja-blog/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<add-blog></add-blog>
+	</div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+
+export default {
+	components: {
+		'add-blog': addBlog
+	},
+	data() {
+		return {
+
+		}
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Nunito');
+
+body{
+	margin: 0;
+	font-family: 'Nunito';
+}
+</style>
