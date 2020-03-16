cc77d45896bfa684afe8c432620f890b4bcd17cb
pochiman <pochiman@yahoo.com>
2020-03-15 21:29:40 -0400

    33

--- ninja-blog/src/components/addBlog.vue
+++ ninja-blog/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="add-blog">
 		<h2>Add a New Blog Post</h2>
-		<form>
+		<form v-if="!submitted">
 			<label>Blog Title:</label>
 			<input type="text" v-model.lazy="blog.title" required />
 			<label>Blog Content:</label>
@@ -20,7 +20,11 @@
 			<select v-model="blog.author">
 				<option v-for="author in authors">{{ author }}</option>
 			</select>
+			<button v-on:click.prevent="post">Add Blog</button>
 		</form>
+		<div v-if="submitted">
+			<h3>Thanks for adding your post</h3>
+		</div>
 		<div id="preview">
 			<h3>Preview Blog</h3>
 			<p>Blog title: {{ blog.title }}</p>
@@ -48,11 +52,21 @@ export default {
 				categories: [],
 				author: ""
 			},
-			authors: ['The React Reaper', 'The Angular Avenger', 'The Vue Vindicator']
+			authors: ['The React Reaper', 'The Angular Avenger', 'The Vue Vindicator'],
+			submitted: false,
 		}
 	},
 	methods: {
-
+		post: function(){
+			this.$http.post('http://jsonplaceholder.typicode.com/posts', {
+				title: this.blog.title,
+				body: this.blog.content,
+				userId: 1
+			}).then(function(data){
+				console.log(data);
+				this.submitted = true;
+			});
+		}
 	}
 }
 </script>
